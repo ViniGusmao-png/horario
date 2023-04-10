0dfd92b0faad582b5a8401baf3ca0a5c17c154ae
Vinicius <vinicius.gusmao.hxh@outlook.com>
2023-04-10 18:46:05 -0300

    Correções

--- hora.js
+++ hora.js
@@ -2,18 +2,19 @@ function carregar(){
     var msg = document.getElementById("mensagem")
     var img = document.getElementById("foto")
     var dia = new Date()
-    var hora = dia.getHours()
+    //var hora = dia.getHours()
+    var hora = 19
     var min = dia.getMinutes()
     msg.innerHTML = `Agora são ${hora}:${min}`
     if ( hora > 0 && hora <= 12){
-        img.src="dia.png"
-        document.body.style.background = '#F2CD5C'
+        img.src="bomdia.png"
+        document.body.style.background = '#F2DAC4'
     }else if( hora > 12 && hora <= 18){
-        img.src="tarde.png"
-        document.body.style.background = '#F2F2F2'
+        img.src="boatarde.png"
+        document.body.style.background = '#2192BF'
     }else{
-        img.src="noite.png"
-        document.body,style.background ='#131240'
+        img.src="boanoite.png"
+        document.body.style.background ='#8C2703'
     }
 
 
